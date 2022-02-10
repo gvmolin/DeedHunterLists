6c476ab77a7b1c979a175b3dd489b8b4473f4fd7
gvmolin <molingv@hotmail.com>
2022-02-10 17:26:51 -0300

    iniciando programa mapa, json dos estados inserido

---
+++ public/js/controllers/mapControllers.js
@@ -0,0 +1,11 @@
+import * as manager from "../programs/program.js";
+
+const states = document.querySelectorAll('path')
+//console.log(states)
+for (let i = 0; i < states.length; i++){
+    const stateIndex = states[i]
+    const stateName = stateIndex.id
+    stateIndex.addEventListener('click', ()=>{
+        
+    })
+}
